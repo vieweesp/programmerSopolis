bb0d42164b33966de13c5e83d33f10d798b569aa
egarcia <egarcia@gtssl.com>
2022-05-12 14:05:25 +0200

    35 Ajustar configuración Axios para enviar token en cada petición

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 import { Constants } from "expo-constants";
+import { USER_TOKEN_KEY } from "../providers/AuthProvider";
 
 
 const axiosInstance = axios.create({
@@ -11,6 +12,13 @@ const axiosInstance = axios.create({
         'Content-Type': 'application/json',
         accept: 'application/json',
     }
+});
+
+//configura axios para cada peticion utilice el token del usuario guardado en storage
+axiosInstance.interceptors.request.use(async req => {
+    const access_token = await SecureStore.getItemAsync(USER_TOKEN_KEY);
+  req.headers["Authorization"] = `Bearer ${access_token}`;
+  return req;
 });
 
 export default axiosInstance;
